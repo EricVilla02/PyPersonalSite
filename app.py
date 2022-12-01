ab7c11151a3ea97b67e62eeafbda4926ad1decb9
EricVilla02 <112974085+EricVilla02@users.noreply.github.com>
2022-12-01 19:04:23 +0000

    changes 2

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask
+from flask import render_template,request
+
+app = Flask(__name__)
+
+# Function to read in details for page
+def readDetails(filepath):
+    with open(filepath, 'r') as f:
+        return [line for line in f]
+
+
+@app.route('/')
+def homePage():
+    name= "Eric Villa"
+    details = readDetails('static/details.txt')
+    return render_template("base.html", name=name,aboutMe=details)
+
 
